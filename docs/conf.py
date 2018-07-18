90b249a77933c935ee1cb1bcb802f993f0083ee5
Hawk <wanghao.buaa@gmail.com>
2018-07-18 10:01:41 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -181,24 +181,24 @@ htmlhelp_basename = 'ReadtheDocsTemplatedoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
-latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+# latex_elements = {
+# # The paper size ('letterpaper' or 'a4paper').
+# #'papersize': 'letterpaper',
 
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+# # The font size ('10pt', '11pt' or '12pt').
+# #'pointsize': '10pt',
 
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
-}
+# # Additional stuff for the LaTeX preamble.
+# #'preamble': '',
+# }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-  ('index', 'ReadtheDocsTemplate.tex', u'Read the Docs Template Documentation',
-   u'Read the Docs', 'manual'),
-]
+# latex_documents = [
+#   ('index', 'ReadtheDocsTemplate.tex', u'Read the Docs Template Documentation',
+#    u'Read the Docs', 'manual'),
+# ]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
